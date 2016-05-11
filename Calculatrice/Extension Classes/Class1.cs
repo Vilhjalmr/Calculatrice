753e5f27f80c6ef00df88ded0143eda82cb547b0
Liam <Liam@CHARLES>
2016-05-11 23:25:47 +0200

    2016-05-11 Night

---
+++ Calculatrice/Extension Classes/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Calculatrice.Extension_Classes
+{
+    public static class TextBoxExtension
+    {
+        public static bool IsTextBoxEmpty(this TextBox textbox)
+        {
+            return textbox.Text.Trim() == "";
+        }
+    }
+}
